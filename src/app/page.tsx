d61e210de5487664e87d47d32b1c29e9f798aead
Ekrem Sönmezer <ekremsonmezer@gmail.com>
2024-02-24 12:17:59 +0300

    Use card and footer in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,68 +1,11 @@
-"use client"
-
-import React, {useEffect, useState} from "react";
-import type {OgObject} from "open-graph-scraper/dist/lib/types";
-import ErrorMessage from "@/components/ErrorMessage";
-import Input from "@/components/Input";
-import OpengraphDetails from "@/components/OpengraphDetails";
-import OpengraphInfo from "@/components/OpengraphInfo";
-import Heading from "@/components/Heading";
-
-type ApiResult = {
-    Success: boolean;
-    Data: OgObject;
-}
+import Footer from "@/components/Footer";
+import Card from "@/components/Card";
 
 export default function Home() {
-    const [url, setURL] = useState("");
-    const [opengraph, setOpenGraph] = useState<ApiResult>({Success: false, Data: {}});
-    const [error, setError] = useState("");
-
-    useEffect(() => {
-        if (url) {
-            fetch('/api', {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({url})
-            }).then(res => res.json())
-                .then(data => {
-                    if (data.success) {
-                        setError("")
-                        console.log(data.result)
-                        setOpenGraph({Success: data.success, Data: data.result})
-                    } else {
-                        setError("Invalid URL")
-                        setOpenGraph({Success: false, Data: {}})
-                    }
-                }).catch(e => {
-                setError("Invalid URL")
-                setOpenGraph({Success: false, Data: {}})
-            })
-        }
-    }, [url])
-
     return (
         <main className="flex h-screen flex-col items-center justify-center p-5">
-            <div
-                className="flex flex-col w-full max-w-2xl min-h-[30rem] overflow-y-auto border shadow rounded-md p-5 gap-5">
-
-                <Heading>Open Graph Metadata</Heading>
-
-                <Input value={url} setValue={setURL}/>
-
-                {error && <ErrorMessage message={error}/>}
-
-                {opengraph.Success &&
-                    <div className={"flex flex-col gap-7"}>
-                        <OpengraphInfo data={opengraph.Data}/>
-                        <OpengraphDetails data={opengraph.Data}/>
-                    </div>
-                }
-            </div>
-
+            <Card/>
+            <Footer/>
         </main>
     );
 }
